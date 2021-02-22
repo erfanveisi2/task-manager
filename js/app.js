fe16e8210ba583c485862da9c2787379a795734c
erfan veisi <erfanveisi2@gmail.com>
2021-02-22 18:02:10 +0300

    dynamic add task is updated

--- js/app.js
+++ js/app.js
@@ -5,3 +5,18 @@ function addTaskBtn() {
 function addTaskInp() {
     return createElement('input', { class: 'add-task-inp hidden', placeholder: 'Enter list title...' })
 }
+
+function mainTask() {
+    return createElement('div', { class: 'task' })
+}
+
+function mainTitle() {
+    var div = createElement('div', { class: 'title' })
+    var h3 = createElement('h3', { class: 'title=h3 left' })
+    var icon = createElement('i', { class: 'fas fa-ellipsis-h menuicon right' })
+
+    div.appendChild(h3)
+    div.appendChild(icon)
+
+    return div
+}
--- js/view.js
+++ js/view.js
@@ -1,4 +1,4 @@
-function dyAddTask() {
+function addTask() {
 
     var sec = document.querySelector('.main-add-task')
 
@@ -6,5 +6,29 @@ function dyAddTask() {
     sec.appendChild(addTaskInp())
 
     return sec
+}
+addTask()
+
+function dyAddTask() {
+    var sec = document.querySelector('.main-add-task')
+    var btn = document.querySelector('.add-task')
+    var inp = document.querySelector('.add-task-inp')
+
+    sec.addEventListener('click', () => {
+        btn.classList.remove('show')
+        btn.classList.add('hidden')
+        inp.classList.remove('hidden')
+        inp.classList.add('show')
+        inp.focus()
+    })
+
+    inp.addEventListener('keyup', (event) => {
+        if (event.keyCode === 13) {
+            inp.classList.remove('show')
+            inp.classList.add('hidden')
+            btn.classList.remove('hidden')
+            btn.classList.add('show')
+        }
+    })
 }
 dyAddTask()
