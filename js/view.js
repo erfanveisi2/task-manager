67e9a73764647c2763b29a6279c6b33e3e3fd33d
erfan veisi <erfanveisi2@gmail.com>
2021-02-25 14:46:04 +0300

    this is the first version of tak section but still has many bugs

--- js/view.js
+++ js/view.js
@@ -16,6 +16,44 @@ function addWork() {
     sec.appendChild(task())
 }
 
+function work() {
+
+    var sec = document.querySelector('.add-work-div')
+    var workSec = document.querySelector('.main-works')
+
+    workSec.appendChild(works())
+    sec.before(workSec)
+
+    return sec
+}
+
+
+function dyAddWork() {
+
+    var sec = document.querySelector('.add-work-div')
+    var btn = document.querySelector('.add-work-btn')
+    var inp = document.querySelector('.add-work-inp')
+
+    sec.addEventListener('click', () => {
+        btn.classList.remove('show')
+        btn.classList.add('hidden')
+        inp.classList.remove('hidden')
+        inp.classList.add('show')
+        inp.focus()
+    })
+
+    inp.addEventListener('keyup', (event) => {
+        if (event.keyCode === 13) {
+            inp.classList.remove('show')
+            inp.classList.add('hidden')
+            btn.classList.remove('hidden')
+            work()
+            inp.value = ''
+        }
+    })
+
+}
+
 function dyAddTask() {
     var sec = document.querySelector('.main-add-task')
     var btn = document.querySelector('.add-task')
@@ -34,6 +72,7 @@ function dyAddTask() {
             inp.classList.add('hidden')
             btn.classList.remove('hidden')
             addWork()
+            dyAddWork()
             inp.value = ''
 
         }
