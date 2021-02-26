3af6741bf3cb06df7c81dbf43e28f0e90e6a03b7
erfan veisi <erfanveisi2@gmail.com>
2021-02-26 15:17:59 +0300

    application file rewrited

--- js/app.js
+++ js/app.js
@@ -5,3 +5,39 @@ function addTaskBtn() {
 function addTaskInp() {
     return createElement('input', { class: 'add-task-inp hidden', placeholder: 'Enter list title...' })
 }
+
+function mainDiv() {
+    return createElement('div', { class: 'task' }, '')
+}
+
+function firstSec() {
+    var titleDiv = createElement('div', { class: 'title' }, '')
+    var titleH3 = createElement('h3', { class: 'title-h3 left' }, '')
+    var titleIcon = createElement('i', { class: 'fas fa-ellipsis-h menuicon right' }, '')
+
+    titleDiv.appendChild(titleH3)
+    titleDiv.appendChild(titleIcon)
+
+    return mainDiv().appendChild(titleDiv)
+}
+
+function seSec() {
+    var worksDiv = createElement('div', { class: 'works' }, '')
+    var worksNote = createElement('p', { class: 'workd-note' }, )
+
+    worksDiv.appendChild(worksNote)
+    return mainDiv().appendChild(worksDiv)
+}
+
+function thirdSec() {
+    var addWorkDiv = createElement('div', { class: 'add-work-div' }, 'Add another card')
+    var addWorkBtn = createElement('button', { class: 'add-work-btn show' })
+    var addWorkIcon = createElement('i', { class: 'fas fa-plus' }, '')
+    var addWorkInp = createElement('input', { class: 'add-work-inp- hidden', placeholder: 'Enter a title for this card...' })
+
+    addWorkDiv.appendChild(addWorkIcon)
+    addWorkDiv.appendChild(addWorkBtn)
+    addWorkDiv.appendChild(addWorkInp)
+
+    return mainDiv().appendChild(addWorkDiv)
+}
