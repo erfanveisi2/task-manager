53f95543376ba381a0829cdc8a91efc0d3ee79d4
erfan veisi <erfanveisi2@gmail.com>
2021-02-27 10:52:11 +0300

    card section is dynamic now

--- js/app.js
+++ js/app.js
@@ -7,34 +7,28 @@ function addCardInp() {
 }
 
 
-function firstSec() {
+function mainCard() {
+    var mainDiv = createElement('div', { class: 'task' }, '')
     var titleDiv = createElement('div', { class: 'title' }, '')
     var titleH3 = createElement('h3', { class: 'title-h3 left' }, '')
     var titleIcon = createElement('i', { class: 'fas fa-ellipsis-h menuicon right' }, '')
-
-    titleDiv.appendChild(titleH3)
-    titleDiv.appendChild(titleIcon)
-
-    return titleDiv
-}
-
-function seSec() {
     var worksDiv = createElement('div', { class: 'works' }, '')
     var worksNote = createElement('p', { class: 'workd-note' }, )
-
-    worksDiv.appendChild(worksNote)
-    return worksDiv
-}
-
-function thirdSec() {
     var addWorkDiv = createElement('div', { class: 'add-work-div' }, '')
     var addWorkBtn = createElement('button', { class: 'add-work-btn show' }, 'Add another card')
     var addWorkIcon = createElement('i', { class: 'fas fa-plus' }, '')
     var addWorkInp = createElement('input', { class: 'add-work-inp- hidden', placeholder: 'Enter a title for this card...' })
 
+    titleDiv.appendChild(titleH3)
+    titleDiv.appendChild(titleIcon)
+    worksDiv.appendChild(worksNote)
     addWorkBtn.appendChild(addWorkIcon)
     addWorkDiv.appendChild(addWorkBtn)
     addWorkDiv.appendChild(addWorkInp)
 
-    return addWorkDiv
+    mainDiv.appendChild(titleDiv)
+    mainDiv.appendChild(worksDiv)
+    mainDiv.appendChild(addWorkDiv)
+
+    return mainDiv
 }
--- js/view.js
+++ js/view.js
@@ -8,15 +8,10 @@ addCard()
 
 function card() {
     var sec = document.querySelector('.main-task')
-    var div = document.querySelector('.task')
 
-    div.appendChild(firstSec())
-    div.appendChild(seSec())
-    div.appendChild(thirdSec())
-
-    sec.appendChild(div)
+    sec.appendChild(mainCard())
 }
-card()
+
 
 function dyAddCard() {
     var sec = document.querySelector('.main-add-task')
@@ -36,6 +31,7 @@ function dyAddCard() {
             inp.classList.remove('show')
             btn.classList.remove('hidden')
             btn.classList.add('show')
+            card()
             inp.value = ''
         }
     })
