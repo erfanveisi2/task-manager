ad066b9e3b72dc01448bd3fc43e4732371371967
erfan veisi <erfanveisi2@gmail.com>
2021-02-27 16:15:41 +0300

    add work is dynamic now

--- js/app.js
+++ js/app.js
@@ -6,29 +6,35 @@ function addCardInp() {
     return createElement('input', { class: 'add-task-inp hidden', placeholder: 'Enter list title...' })
 }
 
-
-function mainCard() {
-    var mainDiv = createElement('div', { class: 'task' }, '')
-    var titleDiv = createElement('div', { class: 'title' }, '')
-    var titleH3 = createElement('h3', { class: 'title-h3 left' }, '')
-    var titleIcon = createElement('i', { class: 'fas fa-ellipsis-h menuicon right' }, '')
-    var worksDiv = createElement('div', { class: 'works' }, '')
-    var worksNote = createElement('p', { class: 'workd-note' }, )
+function addWork() {
     var addWorkDiv = createElement('div', { class: 'add-work-div' }, '')
     var addWorkBtn = createElement('button', { class: 'add-work-btn show' }, 'Add another card')
     var addWorkIcon = createElement('i', { class: 'fas fa-plus' }, '')
-    var addWorkInp = createElement('input', { class: 'add-work-inp- hidden', placeholder: 'Enter a title for this card...' })
+    var addWorkInp = createElement('input', { class: 'add-work-inp hidden', placeholder: 'Enter a title for this card...' })
+
+    addWorkBtn.appendChild(addWorkIcon)
+    addWorkDiv.appendChild(addWorkBtn)
+    addWorkDiv.appendChild(addWorkInp)
+    return addWorkDiv
+}
+
+function mainCard() {
+    var inp = document.querySelector('.add-task-inp')
+
+    var mainDiv = createElement('div', { class: 'task' }, '')
+    var titleDiv = createElement('div', { class: 'title' }, '')
+    var titleH3 = createElement('h3', { class: 'title-h3 left' }, inp.value)
+    var titleIcon = createElement('i', { class: 'fas fa-ellipsis-h menuicon right' }, '')
+    var worksDiv = createElement('div', { class: 'works' }, '')
+    var worksNote = createElement('p', { class: 'work-note' }, )
 
     titleDiv.appendChild(titleH3)
     titleDiv.appendChild(titleIcon)
     worksDiv.appendChild(worksNote)
-    addWorkBtn.appendChild(addWorkIcon)
-    addWorkDiv.appendChild(addWorkBtn)
-    addWorkDiv.appendChild(addWorkInp)
 
     mainDiv.appendChild(titleDiv)
     mainDiv.appendChild(worksDiv)
-    mainDiv.appendChild(addWorkDiv)
+    mainDiv.appendChild(addWork())
 
     return mainDiv
 }
--- js/view.js
+++ js/view.js
@@ -10,6 +10,27 @@ function card() {
     var sec = document.querySelector('.main-task')
 
     sec.appendChild(mainCard())
+
+    var mainDiv = document.querySelector('.add-work-div')
+    var btn = document.querySelector('.add-work-btn')
+    var inp = document.querySelector('.add-work-inp')
+
+    mainDiv.addEventListener('click', () => {
+        btn.classList.add('hidden')
+        btn.classList.remove('show')
+        inp.classList.remove('hidden')
+        inp.classList.add('show')
+        inp.focus()
+    })
+    inp.addEventListener('keyup', (event) => {
+        if (event.keyCode === 13) {
+            inp.classList.add('hidden')
+            inp.classList.remove('show')
+            btn.classList.remove('hidden')
+            btn.classList.add('show')
+            inp.value = ''
+        }
+    })
 }
 
 
